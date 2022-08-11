5b7202009ce9049f38ff77f7cbc5ec2e250e3225
OlenaYatseniuk <102380564+OlenaYatseniuk@users.noreply.github.com>
2022-08-11 20:04:57 +0000

    Deploying to gh-pages from @ OlenaYatseniuk/goit-js-hw-09@4f24b91829389e803becfe8c20f88b44d656c7c9 🚀

---
+++ 01-color-switcher.1811df14.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");e.disabled=!0;var a=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){document.body.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.1811df14.js.map
